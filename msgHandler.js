37fe5c74315349d808a58396a9aa1f5754efddb1
Yusakha <yogafirdaus45421@gmail.com>
2021-02-18 12:36:32 +0700

    Create msgHandler.js
    Base

---
+++ msgHandler.js
@@ -0,0 +1,94 @@
+require('dotenv').config()
+const { decryptMedia } = require('@open-wa/wa-decrypt')
+const moment = require('moment-timezone')
+const { msgFilter, color, processTime, is } = require('./etc/func.js')
+moment.tz.setDefault('Asia/Jakarta').locale('id')
+
+module.exports = msgHandler = async (client, message) => {
+    try {
+        const { type, id, from, t, sender, isGroupMsg, chat, author, caption, isMedia, isGif, mimetype, quotedMsg, quotedMsgObj, mentionedJidList } = message
+        const { name, formattedTitle } = chat
+        let { pushname, verifiedName, formattedName} = sender
+        pushname = pushname || verifiedName || formattedName
+
+        // Bot Prefix
+        const prefix = '#'
+        let { body } = message
+        body = (type === 'chat' && body.startsWith(prefix)) ? body : (((type === 'image' || type === 'video') && caption) && caption.startsWith(prefix)) ? caption : ''
+        const command = body.slice(1).trim().split(/ +/).shift().toLowerCase()
+        const arg = body.substring(body.indexOf(' ') + 1)
+        const args = body.trim().split(/ +/).slice(1)
+        const isCmd = body.startsWith(prefix)
+        const isQuotedImage = quotedMsg && quotedMsg.type === 'image'
+        const url = args.length !== 0 ? args[0] : ''
+        const uaOverride = process.env.UserAgent
+
+        //
+        // const botNumber = await client.getHostNumber() + '@c.us'
+        const groupId = isGroupMsg ? chat.groupMetadata.id : ''
+        const groupAdmins = isGroupMsg ? await client.getGroupAdmins(groupId) : ''
+        const groupMembers = isGroupMsg ? await client.getGroupMembersId(groupId) : ''
+        const botNumber = await client.getHostNumber()
+        const blockNumber = await client.getBlockedIds()
+        const isGroupAdmins = isGroupMsg ? groupAdmins.includes(sender.id) : false
+        const isBotGroupAdmins = isGroupMsg ? groupAdmins.includes(botNumber + '@c.us') : false
+        const ownerNumber = ["XXXX@c.us","XXXXX"] // replace with your whatsapp number
+        const isOwner = ownerNumber.includes(sender.id)
+        const isBlocked = blockNumber.includes(sender.id)
+
+        // [BETA] Avoid Spam Message
+        if (isCmd && msgFilter.isFiltered(from) && !isGroupMsg) { return console.log(color('[SPAM]', 'red'), color(moment(t * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), color(`${command} [${args.length}]`), 'from', color(pushname)) }
+        if (isCmd && msgFilter.isFiltered(from) && isGroupMsg) { return console.log(color('[SPAM]', 'red'), color(moment(t * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), color(`${command} [${args.length}]`), 'from', color(pushname), 'in', color(name || formattedTitle)) }
+        //
+        if (!isCmd && !isGroupMsg) { return console.log('[RECV]', color(moment(t * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), 'Message from', color(pushname)) }
+        if (!isCmd && isGroupMsg) { return console.log('[RECV]', color(moment(t * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), 'Message from', color(pushname), 'in', color(name || formattedTitle)) }
+        if (isCmd && !isGroupMsg) { console.log(color('[EXEC]'), color(moment(t * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), color(`${command} [${args.length}]`), 'from', color(pushname)) }
+        if (isCmd && isGroupMsg) { console.log(color('[EXEC]'), color(moment(t * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), color(`${command} [${args.length}]`), 'from', color(pushname), 'in', color(name || formattedTitle)) }
+        // [BETA] Avoid Spam Message
+        msgFilter.addFilter(from)
+    
+        //
+        const mess = {
+            wait: '[ WAIT ] Sedang di proses⏳ silahkan tunggu sebentar',
+            error: {
+                St: '[❗] Kirim gambar dengan caption *!sticker* atau tag gambar yang sudah dikirim',
+                Ki: '[❗] Bot tidak bisa mengeluarkan admin group!',
+                Ad: '[❗] Tidak dapat menambahkan target, mungkin karena di private',
+                Iv: '[❗] Link yang anda kirim tidak valid!'
+            }
+        }
+
+        if (isBlocked) return
+        //if (!isOwner) return
+        switch(command) {
+        case 'sticker':
+        case 'stiker':
+        case 's':
+            if (isMedia && type === 'image') {
+                const mediaData = await decryptMedia(message, uaOverride)
+                const imageBase64 = `data:${mimetype};base64,${mediaData.toString('base64')}`
+                await client.sendImageAsSticker(from, imageBase64).then(() => console.log(`Sticker Processed for ${processTime(t, moment())} Second`))
+            } else if (quotedMsg && quotedMsg.type == 'image') {
+                const mediaData = await decryptMedia(quotedMsg, uaOverride)
+                const imageBase64 = `data:${quotedMsg.mimetype};base64,${mediaData.toString('base64')}`
+                await client.sendImageAsSticker(from, imageBase64).then(() => console.log(`Sticker Processed for ${processTime(t, moment())} Second`))
+            } else if (args.length === 2) {
+                const url = args[1]
+                if (url.match(isUrl)) {
+                    await client.sendStickerfromUrl(from, url, { method: 'get' }).then(() => console.log(`Sticker Processed for ${processTime(t, moment())} Second`))
+                        .catch(err => console.log('Caught exception: ', err))
+                } else {
+                    client.reply(from, mess.error.Iv, id)
+                }
+            } else {
+                    client.reply(from, mess.error.St, id)
+            }
+            break
+        default:
+            console.log(color('[ERROR]', 'red'), color(moment(t * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), 'Unregistered Command from', color(pushname))
+            break
+        }
+    } catch (err) {
+        console.error(color(err, 'red'))
+    }
+}
