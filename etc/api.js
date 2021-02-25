8b16b08e5f2d13eea5c96d89e00d1306781ff07a
Yusakha <yogafirdaus45421@gmail.com>
2021-02-25 18:33:39 +0700

    Update api.js

--- etc/api.js
+++ etc/api.js
@@ -1,4 +1,13 @@
 const { fetchJson, fetchBase64 } = require('./fetcher.js')
+const axios = require('axios')
+// const fetch = require('node-fetch')
+
+// api
+// Aruga : https://arugaz.herokuapp.com/
+
+// https://mhankbarbar.herokuapp.com/api
+// https://scrap.terhambar.com/
+
 
 /**
  * create custom meme
@@ -17,7 +26,10 @@ const custom = async (imageUrl, top, bottom) => new Promise((resolve, reject) =>
         })
 })
 
-const sreddit = async (reddit) => new Promise((resolve, reject) => {
+const sreddit = async (argument) => new Promise((resolve, reject) => {
+    if (typeof argument == "object") {reddit = argument[Math.random() * argument.length | 0]}
+    else reddit = argument
+    console.log(`looking for ${argument} picture on reddit` )
     fetchJson('https://meme-api.herokuapp.com/gimme/' + reddit + '/')
       .then((rest) => {
           resolve(rest)
@@ -27,7 +39,52 @@ const sreddit = async (reddit) => new Promise((resolve, reject) => {
       })
 })
 
+const pinterest = async (argument) => new Promise((resolve, reject) => {
+    if (typeof argument == "object") {arguments = argument[Math.random() * argument.length | 0]}
+    else arguments = argument
+    console.log(`looking for ${arguments} picture on pinterest` )
+    var urlshot = "https://api.fdci.se/rep.php?gambar=" + arguments
+    axios.get(urlshot).then((result) => {
+    var pictures = JSON.parse(JSON.stringify(result.data))
+    var pict =  pictures[Math.floor(Math.random() * pictures.length)]
+    resolve(pict)
+    })
+    .catch(
+        (error) => {
+            console.log(error)
+            reject(error)
+      })
+})
+
+const mhankbarbar = async (argument) => new Promise((resolve, reject) => {
+    console.log(`looking for ${argument} picture on mhankbarbar` )
+    fetchJson('https://mhankbarbar.herokuapp.com/api/' + argument)
+      .then((rest) => {
+          resolve(rest)
+   })
+      .catch((errr) => {
+          reject(errr)
+      })
+})
+
+const computerfreaker = async (argument) => new Promise((resolve, reject) => {
+    console.log(`looking for ${argument} picture on https://api.computerfreaker.cf/` )
+    const url = 'https://api.computerfreaker.cf/v1/' + argument
+    axios.get(url,{method: 'get',headers: {'user-agent': 'chi-whatsapp-bot'}})
+      .then((result) => {
+            resolve(result.data.url)
+  })
+      .catch(
+        (error) => {
+            console.log(error)
+            reject(error)
+      })
+})
+
 module.exports = {
     custom,
-    sreddit
+    sreddit,
+    pinterest,
+    mhankbarbar,
+    computerfreaker
 }
